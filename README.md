293a671b229e02271ea503fdbc2d127f53d129d6
Zamira Rahman <zamirarahman@Zamiras-iMac.fios-router.home>
2022-11-01 15:16:35 -0400

    completed lab

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # gdp-analysis
+def get_highest_gdp(data, year):
+        maximum = float(data[0][year])
+        for row in data:
+            value = float(data["2020"])
+            if value > maximum:
+                maximum = value
+        return maximum
+        
+
+    def get_lowest_gdp(data, year):
+        minimum = float(data[0][year])
+        for row in data:
+            value = float(row["2020"])
+            if value < minimum:
+                minimum = value
+        return minimum
--- analysis.py
+++ analysis.py
@@ -1,21 +1,61 @@
 import csv
+from curses import A_HORIZONTAL
+
 
 def get_highest_gdp(data, year):
-    pass
+        maximum = float(data[0][year])
+        state = ""
+        for row in data:
+            value = float(row["2020"])
+            if value > maximum:
+                maximum = value
+                state = row ["GeoName"]
+        return state
+      
+
 
 def get_lowest_gdp(data, year):
-    pass
+    minimum = float(data[0][year])
+    state= ""
+    for row in data:
+            value = float(row["2020"])
+            if value < minimum:
+                minimum = value
+                state = row ["GeoName"]
+    return state
+    
+
+def get_state_gdp(data, state, year):
+    stae= A_HORIZONTALyear=2000
+    for row in data:
+        if row ["GeoName"] == state:
+         return row[year]
 
 # save each row into a list (TODO: change to your path!)
 list_data = []
-with open("data/state_gdp_analysis.csv", "r") as infile:
+with open("state_gdp_analysis.csv", "r") as infile:
     # load in data as DictReader
     reader = csv.DictReader(infile)
     # go through each year and get highest and lowest gdp
     for row in reader:
         list_data.append(row)
+    
 
 # get highest gdp for 2020 using "get_highest_gdp(list_data, '2020')"
+print(get_highest_gdp(list_data, '2020'))
+print(get_lowest_gdp(list_data, '2020'))
+print(get_state_gdp(list_data, "New York", "2020"))
+states = ["New York", "Arizona", "Tennessee", "Kansas", "Alaska"]
+for geoname in states:
+    print(get_state_gdp(list_data, geoname, "2020"))
+    x= (get_state_gdp(list_data, "New York", "2020"))
+    y= (get_state_gdp(list_data, "New York", "2019"))
 
+    print(float(x)- float(y))
+def get_states_gdp(data, year):
+    for row in data:
+        print (row[year])
+print(get_states_gdp(list_data, "2020"))
+    
 
 # get lowest gdp for 2020 using "get_lowest_gdp(list_data, '2020')"
